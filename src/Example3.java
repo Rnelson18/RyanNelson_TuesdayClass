814deb5664c2baf480ff1ae1d3e2868261b2b7fc
ryannelson <r3nelson@student.bridgew.edu>
2021-09-09 09:49:29 -0400

    Working Example 3

--- src/Example3.java
+++ src/Example3.java
@@ -1,14 +1,22 @@
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Example3 {
     public static void main(String[] args){
-        var studentNames =  new ArrayList<String>();
+        var studentName =  new ArrayList<String>();
         var reader = new Scanner(System.in);
         var userResponse = "";
         while(!userResponse.equals("done")){
             System.out.println("Enter next name or 'done' tp end:");
             userResponse = reader.nextLine();
             if (userResponse.equals("done")){
-                studentNames.add(userResponse);
+                studentName.add(userResponse);
             }
+        }
+        System.out.println("The students in your class are: ");
+        for (var Name: studentName){
+            System.out.println(Name);
         }
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         var reader = new Scanner(System.in);
         System.out.print("How old are you:");
-        var age float = reader.nextFloat();
+        var age; float = reader.nextFloat();
         var dogYears = age+7;
         System.out.println("Wow you are "age+ " that is " + dogYears " in dog years");
 
